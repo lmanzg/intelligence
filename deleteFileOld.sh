070298b8e147f8bf352e1564f06c0342f654c5d8
lmanzg <51437598+lmanzg@users.noreply.github.com>
2021-01-15 14:25:06 +0800

    deleteFileOld.sh 增加参数3 参数3 不为空 参数2为天数 参数3 为空  参数2为个数

--- deleteFileOld.sh
+++ deleteFileOld.sh
@@ -1,27 +1,41 @@
-deleteNum=$2
-
-FileName="/mnt/mmc/log/remainingFileSize.log"
-
-if [ -f "$FileName" ]; then
-	FileSize=`ls -l $FileName | awk '{ print $5 }'`
-	MaxSize=$((10240*10))
-	if [ $FileSize -gt $MaxSize ]; then
-		rm $FileName
-	else
-		echo "$FileSize < $MaxSize "
-	fi
+if [ $# -lt 1 ]; then
+	echo "error:The parameter is less than 1"
+	exit 1
 fi
+######################################################
+#			根据日志文件大小 删除日志文件
+#
+#FileName="/mnt/mmc/log/remainingFileSize.log"
+#
+#if [ -f "$FileName" ]; then
+#	FileSize=`ls -l $FileName | awk '{ print $5 }'`
+#	MaxSize=$((10240*10))
+#	if [ $FileSize -gt $MaxSize ]; then
+#		rm $FileName
+#	else
+#		echo "$FileSize < $MaxSize "
+#	fi
+#fi
+######################################################
 
 FileDir=$1
+deleteNum=$2
+
 if [ ! -d "$FileDir" ]; then
-	exit 0
+	echo "error:Parameter 1  Path error"
+	exit 1
 fi
 
 cd $FileDir
-
-if [ $3 -eq 0 ]; 
+if [ $# -eq 3 ]; 
 then
     file=` ls -rt  *.log*| head -1 `
+
+    if [ ! -f "$file" ];
+    then
+    	echo "No log File"
+		exit 1
+    fi 
 
     Ltime=`stat $file | grep Modify | awk '{print $2}' `
     echo $Ltime
@@ -34,27 +48,24 @@ then
     dat=` expr $dat / 60 `          #Time
     dat=` expr $dat / 24 `          #day
     dat=` expr $dat - 1 `           #day
-    echo $dat
-    echo $deleteNum
+    
     if [ $deleteNum -le $dat ];
     then
     	dat=$(($dat - $deleteNum)) 			#deleteDay
-    	echo $deleteNum	
-    	echo $dat
+    	
     	deleteNum=` find $FileDir -name "*.log*" -mtime +$dat | wc -l `
-    	echo "-----$deleteNum"
     else	
-    	dat=` expr $dat + 1 `
+    	dat=$deleteNum
     	deleteNum=` find $FileDir -name "*.log*" -mtime -$dat | wc -l `
-    	echo "--2---$deleteNum"
-    fi
+	fi
    
     if [ $deleteNum -le 0 ];
     then
+    	echo "There are no deleted files"
         exit 1
     fi
 fi
-echo 11111
+
 num=0
 while :
 do	
@@ -64,6 +75,7 @@ do
 	OldFile=$(ls -rt  *.log*| head -1)
 
 	if [ ! -f "$OldFile" ]; then
+		echo "no log file"
 		exit 1
 	fi
 
