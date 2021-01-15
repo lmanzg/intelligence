b887284a914a01cc26c1251c10d3b4b4c1bb3f7d
lmanzg <51437598+lmanzg@users.noreply.github.com>
2021-01-15 14:19:13 +0800

    deleteFileOld.sh 增加参数3 参数3 不为空 参数2为天数 参数3 为空  参数2为个数

--- deleteFileOld.sh
+++ deleteFileOld.sh
@@ -1,5 +1,3 @@
-
-FileDir=$1
 deleteNum=$2
 
 FileName="/mnt/mmc/log/remainingFileSize.log"
@@ -14,23 +12,59 @@ if [ -f "$FileName" ]; then
 	fi
 fi
 
-
+FileDir=$1
 if [ ! -d "$FileDir" ]; then
 	exit 0
 fi
 
 cd $FileDir
-echo $FileDir
+
+if [ $3 -eq 0 ]; 
+then
+    file=` ls -rt  *.log*| head -1 `
+
+    Ltime=`stat $file | grep Modify | awk '{print $2}' `
+    echo $Ltime
+    time=`date  '+%Y-%m-%d'`
+  
+    time1=`date +%s -d $time`
+    time2=`date +%s -d $Ltime`
+    dat=` expr $time1 - $time2 `    #second
+    dat=` expr $dat / 60 `          #branch
+    dat=` expr $dat / 60 `          #Time
+    dat=` expr $dat / 24 `          #day
+    dat=` expr $dat - 1 `           #day
+    echo $dat
+    echo $deleteNum
+    if [ $deleteNum -le $dat ];
+    then
+    	dat=$(($dat - $deleteNum)) 			#deleteDay
+    	echo $deleteNum	
+    	echo $dat
+    	deleteNum=` find $FileDir -name "*.log*" -mtime +$dat | wc -l `
+    	echo "-----$deleteNum"
+    else	
+    	dat=` expr $dat + 1 `
+    	deleteNum=` find $FileDir -name "*.log*" -mtime -$dat | wc -l `
+    	echo "--2---$deleteNum"
+    fi
+   
+    if [ $deleteNum -le 0 ];
+    then
+        exit 1
+    fi
+fi
+echo 11111
 num=0
 while :
-do
+do	
 	if [ $num -eq $deleteNum ]; then
-		exit 0	
+		exit 1	
 	fi
 	OldFile=$(ls -rt  *.log*| head -1)
 
 	if [ ! -f "$OldFile" ]; then
-		exit 0
+		exit 1
 	fi
 
 	echo  "Delete File:" $OldFile
@@ -38,4 +72,3 @@ do
 	let num=num+1
 	echo $num
 done
-
